9fa0de42b3ad1d469590f2a435ce38c3fba7923e
Soulhae <benjamin.herrera.a@mail.pucv.cl>
2021-04-21 23:21:59 -0400

    prototipo funciones

---
+++ pokemon.h
@@ -0,0 +1,23 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+#include "map.h"
+
+//typedef struct Pokemon Pokemon;
+
+//typedef struct Pokedex Pokedex;
+
+Pokemon *crearPokemon(char *nombre, char **tipos, int PC, int PS, char *sexo, char *ePrevia, char *ePosterior, int numPokedex, char *region);
+
+void agregarPokemon(HashMap *mapaNombre, HashMap *mapaId, HashMap *mapaTipo, HashMap *mapaRegion, char *nombre, char **tipos, int PC, int PS, char *sexo, char *ePrevia, char *ePosterior, int numPokedex, char *region);
+
+void buscarId(HashMap *mapaId, int id_pokemon);
+
+void buscarTipo(HashMap *mapaTipo);
+
+void buscarNombre(HashMap *mapaNombre);
+
+void buscarNombrePokedex();
+
+void leerArchivo(HashMap *mapaNombre, HashMap *mapaId, HashMap *mapaTipo, HashMap *mapaRegion , char *archivo);
+
+#endif /* POKEMON_H */
